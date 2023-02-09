5d231b4192cba8adcf6093263047ec68dfce98dd
Maria Rumyantseva <manechka96@list.ru>
2023-02-09 14:53:49 +0300

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsStack = [];
+  const ourBrackets = Object.fromEntries(bracketsConfig);
+
+  for (let bracket of str) {
+    let last = bracketsStack[bracketsStack.length - 1];
+    if (bracket in ourBrackets && bracket !== ourBrackets[last]) {
+      bracketsStack.push(bracket);
+    } else if (bracket !== ourBrackets[bracketsStack.pop()]) {
+      return false;
+    }
+  }
+
+  return !bracketsStack.length;
 }
